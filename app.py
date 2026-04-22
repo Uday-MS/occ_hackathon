86381f6c71189b37de9ecaa457d3d3b09af0ebc1
udayms <udayms@Mac.lan>
2026-04-22 20:06:03 +0530

    Fixed app.py

--- app.py
+++ app.py
@@ -751,4 +751,4 @@ if "RENDER" not in os.environ:
 # RUN SERVER
 # =============================================================================
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
